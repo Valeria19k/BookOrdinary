9349d912ac95763b7ceb40c4aa0c43a24384cb99
Valeria19k <71520455+Valeria19k@users.noreply.github.com>
2020-12-09 15:49:41 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Book {
+    public int year;
+    public String title;
+    public String author;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
